f69a462f2071a11f402306169d7c404052517250
gomesuit <gomesuit@gmail.com>
2015-07-02 17:46:13 +0900

    server

--- provision.sh
+++ provision.sh
@@ -13,18 +13,28 @@ chmod 600 id_rsa
 # install chef
 curl -L https://www.chef.io/chef/install.sh | bash
 
-#echo 'export PATH=/opt/chef/embedded/bin:$PATH' >> /root/.bash_profile
-#export PATH=/opt/chef/embedded/bin:$PATH
+echo 'export PATH=/opt/chef/embedded/bin:$PATH' >> /root/.bash_profile
+export PATH=/opt/chef/embedded/bin:$PATH
 
 # git clone provisioner
 cd /home/vagrant
-git clone https://github.com/gomesuit/provisioner_chef_zero.git
+git clone https://github.com/gomesuit/provisioner_chef_server.git
 
 # run chef
-cd /home/vagrant/provisioner_chef_zero
+cd /home/vagrant/provisioner_chef_server
 #bundle install
 #bundle exec knife zero bootstrap localhost
 #bundle exec berks vendor cookbooks
 #bundle exec knife node run_list add ansible zsh
 #bundle exec knife zero chef_client 'name:ansible' --attribute ipaddress
-chef-client -z
+#chef-client -z
+
+
+yum insyall -y https://web-dl.packagecloud.io/chef/stable/packages/el/6/chef-server-core-12.1.0-1.el6.x86_64.rpm
+#yum install -y https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.6.2-1.el6.x86_64.rpm
+chef-server-ctl reconfigure
+chef-server-ctl test
+#chef-server-ctl user-create admin firstname lastname your@mail.address password --filename admin.pem
+#chef-server-ctl org-create chef "Chef" --association admin --filename chef-validator.pem
+knife cookbook upload learn_chef_httpd
+knife bootstrap localhost -N localhost --run-list 'recipe[learn_chef_httpd]'
